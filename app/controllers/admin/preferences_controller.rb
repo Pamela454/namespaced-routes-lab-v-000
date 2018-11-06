1ba8d11654205f48eb2de63003e2a679add472f7
Pamela Frieze <pfrieze@gmail.com>
2018-11-06 16:33:16 +0000

    automatically backed up by learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -4,5 +4,10 @@ class Admin::PreferencesController < ApplicationController
     @preference = Preference.first_or_create(allow_create_artists: true, allow_create_songs: true, song_sort_order: "DESC", artist_sort_order: "DESC")
   end
 
+private
+
+  def preference_params
+    params.require(:preference).permit(:allow_create_songs, :allow_create_artists, :song_sort_order, :artist_sort_order)
+  end
   
 end
