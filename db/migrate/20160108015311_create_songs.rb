afaba1be63a71c43e98ff77dcb6d8228ec87b864
Pamela Frieze <pfrieze@gmail.com>
2018-11-06 16:14:30 +0000

    automatically backed up by learn

--- db/migrate/20160108015311_create_songs.rb
+++ db/migrate/20160108015311_create_songs.rb
@@ -1,9 +1,15 @@
 class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
-      t.string :title
+      t.string :artist_sort_order
+      
 
       t.timestamps null: false
     end
   end
 end
+
+expect(p.artist_sort_order.class).to eq String
+    expect(p.song_sort_order.class).to eq String
+    expect(p.allow_create_artists).to eq false
+    expect(p.allow_create_songs).to eq true
